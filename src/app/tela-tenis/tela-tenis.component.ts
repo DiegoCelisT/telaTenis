7bc9b0ac7ee0925adf6c777c55528709931728d4
Diego Celis <diegofercelis@gmail.com>
2021-07-24 03:49:22 -0300

    criando as funcoes para o funcionamento do carrousel e o seletor de imagem

--- src/app/tela-tenis/tela-tenis.component.ts
+++ src/app/tela-tenis/tela-tenis.component.ts
@@ -7,9 +7,57 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TelaTenisComponent implements OnInit {
 
+  public channel: any
+  public photoNumber: any
+  public elemento: any
+  public nikeChannel: any
   constructor() { }
 
   ngOnInit(): void {
+    this.channel = "../../assets/nike-1.png"
+    this.photoNumber = 1;
+    this.elemento = 1
   }
+
+  public channeling(){
+    //Limpando primeiro os canais:
+    for (let i = 1; i < 7; i++) {
+      this.nikeChannel = document.getElementById(i.toString());
+      this.nikeChannel.classList.remove('Selected')
+    }
+    //Estabelecendo o canal selecionado:
+    this.nikeChannel = document.getElementById(this.photoNumber);
+    this.nikeChannel.classList.add('Selected')
+    this.channel = "../../assets/nike-"+this.photoNumber+".png";
+  }
+
+  public selectChannel(NUM: Number) {
+    this.photoNumber = NUM
+    this.channeling()
+  }
+
+  public nextChannel (){
+    this.photoNumber = this.photoNumber + 1
+    if (this.photoNumber>6){
+      this.photoNumber = 1
+    }
+    this.channeling()
+  }
+
+  public previousChannel (){
+    this.photoNumber = this.photoNumber - 1
+    if (this.photoNumber<1){
+      this.photoNumber = 6
+    }
+    this.channeling()
+  }
+  
+  
+  // public play (){
+  //   this.nextChannel ()
+  //   for (var start = 1; start < 12; start++)
+  //   setTimeout( () => { this.nextChannel ();  }, 1500 * start);
+  // }
+
 
 }
